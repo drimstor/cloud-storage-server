094b65adea56cccfc161bbd45375d85885b83f70
Vyacheslav Gurin <drimstor@gmail.com>
2023-10-28 15:50:17 +0700

    websockets

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(filePathMiddleware(path.resolve(__dirname, "files")));
 app.use(fileUpload({}));
